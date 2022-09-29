c110538b2834e0576247807b4ef90f46b5493982
BuyseTjorven <tjorven.buyse@student.howest.be>
2022-09-29 10:20:48 +0200

    first commit

---
+++ Models/CalculationRequest.cs
@@ -0,0 +1,10 @@
+
+namespace MCT.functions.Models
+{
+    public class CalculationRequest
+    {
+        public string Operator { get; set; }
+        public int Getal1 { get; set; }
+        public int Getal2 { get; set; }
+    }
+}
---
+++ Models/CalculationResult.cs
@@ -0,0 +1,9 @@
+
+namespace MCT.functions.Models
+{
+    public class CalculationResult
+    {
+        public string Operator {get; set;}
+        public string Result {get; set;}
+    }
+}
